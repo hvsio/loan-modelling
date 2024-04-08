2b0bb8099d8ff1cf2c90ad1623e3a6dd084780bd
hvsio <alicjaharaszczuk@gmail.com>
2024-04-08 15:48:55 +0200

    Change location of dockerfiles

--- airflow/dags/loan_predictor.py
+++ airflow/dags/loan_predictor.py
@@ -1,24 +1,19 @@
 import logging
 from datetime import datetime, timedelta
-
-from datetime import datetime, timedelta
 from airflow import DAG
-import os
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.docker_operator import DockerOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils import dates
 from dotenv import find_dotenv, load_dotenv
-
+import os
 
 logger = logging.getLogger('airflowLogger')
 
 load_dotenv(find_dotenv())
 
 default_args = {
-    
-    'owner': 'alicja', #os.environ.get('airflow_owner'),
+    'owner': os.environ.get('airflow_owner'),
     'description': 'Loan prediction setup',
     'depend_on_past': False,
     'start_date': datetime(2024, 3, 11),
@@ -28,34 +23,39 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
+with DAG(
     dag_id='loans_setup',
-    start_date=dates.days_ago(2),
     default_args=default_args,
-)
+) as dag:
 
-t1 = DockerOperator(
-    task_id='database_creator',
-    image='database_creator',
-    auto_remove=True,
-    #env_file='.env', set this path
-    dag=dag
-)
+    t1 = DockerOperator(
+        task_id='database_creator',
+        image='h4sio/database_creator3.1:1.0.0',
+        mount_tmp_dir=False,
+        auto_remove="force",
+        env_file='/.env',
+        command="echo 'Execution started {{ ds_nodash }}'",
+        dag=dag
+    )
 
-t2 = DockerOperator(
-    task_id='dataset_puller',
-    image='dataset_puller',
-    auto_remove=True,
-    #env_file='.env', set this path
-    dag=dag
-)
+    t2 = DockerOperator(
+        task_id='dataset_puller',
+        image='h4sio/dataset_puller3.1:1.0.0',
+        mount_tmp_dir=False,
+        auto_remove="force",
+        env_file='/.env',
+        command="echo 'Execution started {{ ds_nodash }}'",
+        dag=dag
+    )
 
-t3 = DockerOperator(
-    task_id='data_modifier',
-    image='data_modifier',
-    auto_remove=True,
-    #env_file='.env', set this path
-    dag=dag
-)
+    t3 = DockerOperator(
+        task_id='data_modifier',
+        image='h4sio/data_modifier3.1:1.0.0',
+        mount_tmp_dir=False,
+        auto_remove="force",
+        env_file='/.env',
+        command="echo 'Execution started {{ ds_nodash }}'",
+        dag=dag
+    )
 
 t1 >> t2 >> t3
